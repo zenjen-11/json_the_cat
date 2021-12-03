2f44a856dbd0f0a0057d2e904d8b53109b02aabf
zenjen-11 <jen.nicholls@teksavvy.com>
2021-12-03 00:47:38 +0000

    Implemented callback function.

---
+++ breedFetcher2.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url,function(error, response, body) {
+
+    const data = JSON.parse(body);
+    const cat = data[0];
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (data[0]) {
+      callback(null, data[0].description);
+    }
+    if (!cat) {
+      callback('Cat not found.');
+      return;
+    }
+
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
